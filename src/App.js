9a69c84e9caa3eda45442f83c118418efb72a2fa
Trupti <truptimhaskar11nov@gmail.com>
2022-08-06 15:39:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from "react";
+
+import "./index.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const imgReducer = (state, action) => {
+    switch (action.type) {
+      case "STACK_IMAGES":
+        return { ...state, images: state.images.concat(action.images) };
+      case "FETCHING_IMAGES":
+        return { ...state, fetching: action.fetching };
+      default:
+        return state;
+    }
+  };
+  const [imgData, imgDispatch] = useReducer(imgReducer, {
+    images: [],
+    fetching: true,
+  });
+  // next code block goes here
 }
+// make API calls
+useEffect(() => {
+  imgDispatch({ type: "FETCHING_IMAGES", fetching: true });
+  fetch("https://picsum.photos/v2/list?page=0&limit=10")
+    .then((data) => data.json())
+    .then((images) => {
+      imgDispatch({ type: "STACK_IMAGES", images });
+      imgDispatch({ type: "FETCHING_IMAGES", fetching: false });
+    })
+    .catch((e) => {
+      // handle error
+      imgDispatch({ type: "FETCHING_IMAGES", fetching: false });
+      return e;
+    });
+}, [imgDispatch]);
 
+// next code block goes here
+return (
+  <div className="">
+    <nav className="navbar bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="/#">
+          <h2>Infinite scroll + image lazy loading</h2>
+        </a>
+      </div>
+    </nav>
+    <div id="images" className="container">
+      <div className="row">
+        {imgData.images.map((image, index) => {
+          const { author, download_url } = image;
+          return (
+            <div key={index} className="card">
+              <div className="card-body ">
+                <img alt={author} className="card-img-top" src={download_url} />
+              </div>
+              <div className="card-footer">
+                <p className="card-text text-center text-capitalize text-primary">
+                  Shot by: {author}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  </div>
+);
 export default App;
